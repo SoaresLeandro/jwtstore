90a29a2a457a4c12d7c684ff48276eae53d98d2d
Leandro <lss.91@live.com>
2024-12-26 17:15:41 -0400

    Criando o Projeto

---
+++ JwtStore.Core/SharedContext/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtStore.Core.SharedContext.Entities
+{
+    public abstract class Entity : IEquatable<Guid>
+    {
+        public Guid Id { get; set; }
+
+        public bool Equals(Guid id) => Id == id;
+
+        public override int GetHashCode() => Id.GetHashCode();
+    }
+}
